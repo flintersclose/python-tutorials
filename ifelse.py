f14fb9c087fa698c376a47af2718f410b3614361
Agni <agni@anumacmini.home>
2018-06-27 19:03:55 +0100

    Added an if-else file

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+a = input("Please enter a number?")
+b = input("Please enter another number?")
+
+if a>b :
+    print(a, "is bigger")
+
+elif a==b:
+    print("both numbers are equal")
+    
+else:
+    print(b, "is bigger")
